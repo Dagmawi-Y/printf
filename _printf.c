6f9baa3143a413b97cb1dc2ec9138d84554d390d
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-10-17 15:52:49 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -1,45 +1,89 @@
 #include "main.h"
+
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * custom_printf - A custom printf function that handles various format specifiers.
+ * @format: The format string.
+ * Return: The number of characters printed.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	FormatInfo format_info[] = {
+		{"%s", print_string}, {"%c", print_char},
+		{"%%", print_percent}, {"%i", print_int},
+		{"%d", print_decimal}, {"%r", print_reverse},
+		{"%R", print_rot13}, {"%b", print_binary},
+		{"%u", print_unsigned}, {"%o", print_octal},
+		{"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_special_string}, {"%p", print_pointer}
 	};
 
 	va_list args;
-	int i = 0, j, len = 0;
+	int format_index = 0, total_chars = 0;
 
 	va_start(args, format);
+
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
-Here:
-	while (format[i] != '\0')
+	while (format[format_index] != '\0')
 	{
-		j = 13;
-		while (j >= 0)
+		if (format[format_index] == '%')
 		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			int specifier_index = 0;
+
+			while (specifier_index < 14)
 			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
+				if (starts_with(format + format_index, format_info[specifier_index].format))
+				{
+					total_chars += format_info[specifier_index].printer(args);
+					format_index += string_length(format_info[specifier_index].format);
+					break;
+				}
+				specifier_index++;
 			}
-			j--;
 		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			_putchar(format[format_index]);
+			total_chars++;
+		}
+
+		format_index++;
 	}
+
 	va_end(args);
+	return (total_chars);
+}
+
+/**
+ * starts_with - Check if a string starts with another string.
+ * @str: The string to check.
+ * @prefix: The prefix to check for.
+ * Return: 1 if str starts with prefix, 0 otherwise.
+ */
+int starts_with(const char *str, const char *prefix)
+{
+	while (*prefix)
+	{
+		if (*prefix != *str)
+			return (0);
+		prefix++;
+		str++;
+	}
+	return (1);
+}
+
+/**
+ * string_length - Calculate the length of a string.
+ * @str: The string to measure.
+ * Return: The length of the string.
+ */
+int string_length(const char *str)
+{
+	int len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 	return (len);
 }
