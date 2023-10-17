6187709b0b66ee39f3de6286128d29ece0949d68
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-10-17 15:42:28 -0700

    header fix

--- main.h
+++ main.h
@@ -2,17 +2,24 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
 
 /**
- * struct format - Match the conversion specifiers for printf
- * @id: The format specifier.
- * @f: Pointer to the corresponding printf function.
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
  */
+
 typedef struct format
 {
-    char *id;
-    int (*f)(va_list);
+	char *id;
+	int (*f)();
 } convert_match;
 
 int printf_pointer(va_list val);
@@ -28,8 +35,15 @@ int printf_srev(va_list args);
 int printf_rot13(va_list args);
 int printf_int(va_list args);
 int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
 int printf_char(va_list val);
 int printf_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
