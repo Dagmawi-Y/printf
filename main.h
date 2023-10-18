df2a06c09ced4477db675d44d309b6646b0032b1
Dagmawi-Y <dagmawi.inbox@gmail.com>
2023-10-17 17:23:16 -0700

    header

--- main.h
+++ main.h
@@ -5,42 +5,43 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
+#include <unistd.h>
+
+
 
 /**
- * struct conversion_specifier - match the conversion specifiers for printf
- * @specifier: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @function: type pointer to function for the conversion specifier
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
  *
  */
 
-#include <unistd.h>
-
-typedef struct conversion_specifier
+typedef struct format
 {
-	char *specifier;
-	int (*function)();
-} specifier_handler;
+	char *id;
+	int (*f)();
+} convert_match;
 
-int print_pointer(va_list args);
-int print_hexadecimal_aux(unsigned long int num);
-int print_hexadecimal(va_list args);
-int print_exclusive_string(va_list args);
-int print_octal(va_list args);
-int print_unsigned_decimal(va_list args);
-int print_binary(va_list args);
-int print_reverse_string(va_list args);
-int print_rot13_string(va_list args);
+int print_pointer(va_list val);
+int print_hex_a(unsigned long int num);
+int print_hex(va_list val);
+int print_es(va_list val);
+int print_octal(va_list val);
+int print_unsigned(va_list args);
+int print_binary(va_list val);
+int print_strrev(va_list args);
+int print_r13(va_list args);
 int print_integer(va_list args);
 int print_decimal(va_list args);
-int string_length(char *s);
-int string_copy(char *dest, char *src);
-int custom_string_length(const char *s);
-int custom_string_length(const char *s);
-int print_percent(void);
-int print_character(va_list args);
-int reverse_string(char *s);
-int print_custom_string(va_list args);
-int write_character(char c);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int _strlenc(const char *s);
+int print_37(void);
+int print_charac(va_list val);
+int rev_string(char *s);
+int print_string(va_list val);
+int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
