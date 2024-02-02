272c5ec60d88385c927eacd299297059272d0834
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-02-02 16:20:54 +0300

    flags added

--- _printf.c
+++ _printf.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
-
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -28,6 +27,23 @@ int _printf(const char *format, ...)
     {
         if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' || *(ptr + 1) == '%' || *(ptr + 1) == 'p' || *(ptr + 1) == 'd' || *(ptr + 1) == 'i' || *(ptr + 1) == 'u' || *(ptr + 1) == 'b' || *(ptr + 1) == 'x' || *(ptr + 1) == 'X' || *(ptr + 1) == 'S' || *(ptr + 1) == 'o'))
         {
+            int has_plus = 0;
+            int has_space = 0;
+            int has_hash = 0;
+
+            /* Check for flags '+' (plus), ' ' (space), and '#' (hash) */
+            while (*(ptr + 2) == '+' || *(ptr + 2) == ' ' || *(ptr + 2) == '#')
+            {
+                if (*(ptr + 2) == '+')
+                    has_plus = 1;
+                else if (*(ptr + 2) == ' ')
+                    has_space = 1;
+                else if (*(ptr + 2) == '#')
+                    has_hash = 1;
+
+                ++ptr;
+            }
+
             switch (*(ptr + 1))
             {
                 case 'c':
@@ -44,7 +60,6 @@ int _printf(const char *format, ...)
                 case 'p':
                     addr = va_arg(args, void *);
                     {
-                        /* Assuming a reasonable buffer size */
                         char buffer[20];
                         sprintf(buffer, "%p", addr);
                         count += write(1, buffer, strlen(buffer));
@@ -53,16 +68,19 @@ int _printf(const char *format, ...)
                 case 'd':
                 case 'i':
                 {
-                    /* Assuming a reasonable buffer size */
                     char buffer[20];
                     int num = va_arg(args, int);
+                    if (has_plus && num >= 0)
+                        count += write(1, "+", 1);
+                    else if (has_space && num >= 0)
+                        count += write(1, " ", 1);
+
                     sprintf(buffer, "%d", num);
                     count += write(1, buffer, strlen(buffer));
                 }
                     break;
                 case 'u':
                 {
-                    /* Assuming a reasonable buffer size */
                     char buffer[20];
                     unsigned int num = va_arg(args, unsigned int);
                     sprintf(buffer, "%u", num);
@@ -71,7 +89,6 @@ int _printf(const char *format, ...)
                     break;
                 case 'b':
                 {
-                    /* Assuming a reasonable buffer size */
                     char buffer[33];
                     unsigned int num = va_arg(args, unsigned int);
                     itoa_binary(num, buffer);
@@ -81,15 +98,27 @@ int _printf(const char *format, ...)
                 case 'x':
                 case 'X':
                 {
-                    /* Assuming a reasonable buffer size */
-
                     unsigned int num = va_arg(args, unsigned int);
+                    if (has_hash && num != 0)
+                        count += write(1, (*(ptr + 1) == 'X') ? "0X" : "0x", 2);
+
                     count += print_hex(num, (*(ptr + 1) == 'X') ? 1 : 0);
                 }
                     break;
                 case 'S':
                     str = va_arg(args, char *);
                     count += print_custom_string(str);
+                    break;
+                case 'o':
+                {
+                    char buffer[20];
+                    unsigned int num = va_arg(args, unsigned int);
+                    if (has_hash && num != 0)
+                        count += write(1, "0", 1);
+
+                    sprintf(buffer, "%o", num);
+                    count += write(1, buffer, strlen(buffer));
+                }
                     break;
 
                 default:
