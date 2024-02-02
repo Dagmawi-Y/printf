e130bbfb15eedbafa14a0499e3d5e7cf2cb3741c
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-02-02 16:25:10 +0300

    flags added

--- _printf.c
+++ _printf.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -27,20 +28,16 @@ int _printf(const char *format, ...)
     {
         if (*ptr == '%' && (*(ptr + 1) == 'c' || *(ptr + 1) == 's' || *(ptr + 1) == '%' || *(ptr + 1) == 'p' || *(ptr + 1) == 'd' || *(ptr + 1) == 'i' || *(ptr + 1) == 'u' || *(ptr + 1) == 'b' || *(ptr + 1) == 'x' || *(ptr + 1) == 'X' || *(ptr + 1) == 'S' || *(ptr + 1) == 'o'))
         {
-            int has_plus = 0;
-            int has_space = 0;
-            int has_hash = 0;
+            int flag_plus = 0;
+            int flag_space = 0;
 
-            /* Check for flags '+' (plus), ' ' (space), and '#' (hash) */
-            while (*(ptr + 2) == '+' || *(ptr + 2) == ' ' || *(ptr + 2) == '#')
+            /* Check for flags */
+            while (*(ptr + 1) == '+' || *(ptr + 1) == ' ')
             {
-                if (*(ptr + 2) == '+')
-                    has_plus = 1;
-                else if (*(ptr + 2) == ' ')
-                    has_space = 1;
-                else if (*(ptr + 2) == '#')
-                    has_hash = 1;
-
+                if (*(ptr + 1) == '+')
+                    flag_plus = 1;
+                else if (*(ptr + 1) == ' ')
+                    flag_space = 1;
                 ++ptr;
             }
 
@@ -60,6 +57,7 @@ int _printf(const char *format, ...)
                 case 'p':
                     addr = va_arg(args, void *);
                     {
+                        /* Assuming a reasonable buffer size */
                         char buffer[20];
                         sprintf(buffer, "%p", addr);
                         count += write(1, buffer, strlen(buffer));
@@ -68,19 +66,16 @@ int _printf(const char *format, ...)
                 case 'd':
                 case 'i':
                 {
+                    /* Assuming a reasonable buffer size */
                     char buffer[20];
                     int num = va_arg(args, int);
-                    if (has_plus && num >= 0)
-                        count += write(1, "+", 1);
-                    else if (has_space && num >= 0)
-                        count += write(1, " ", 1);
-
-                    sprintf(buffer, "%d", num);
+                    sprintf(buffer, (flag_plus ? "%+d" : (flag_space ? "% d" : "%d")), num);
                     count += write(1, buffer, strlen(buffer));
                 }
                     break;
                 case 'u':
                 {
+                    /* Assuming a reasonable buffer size */
                     char buffer[20];
                     unsigned int num = va_arg(args, unsigned int);
                     sprintf(buffer, "%u", num);
@@ -89,6 +84,7 @@ int _printf(const char *format, ...)
                     break;
                 case 'b':
                 {
+                    /* Assuming a reasonable buffer size */
                     char buffer[33];
                     unsigned int num = va_arg(args, unsigned int);
                     itoa_binary(num, buffer);
@@ -98,27 +94,15 @@ int _printf(const char *format, ...)
                 case 'x':
                 case 'X':
                 {
-                    unsigned int num = va_arg(args, unsigned int);
-                    if (has_hash && num != 0)
-                        count += write(1, (*(ptr + 1) == 'X') ? "0X" : "0x", 2);
+                    /* Assuming a reasonable buffer size */
 
+                    unsigned int num = va_arg(args, unsigned int);
                     count += print_hex(num, (*(ptr + 1) == 'X') ? 1 : 0);
                 }
                     break;
                 case 'S':
                     str = va_arg(args, char *);
                     count += print_custom_string(str);
-                    break;
-                case 'o':
-                {
-                    char buffer[20];
-                    unsigned int num = va_arg(args, unsigned int);
-                    if (has_hash && num != 0)
-                        count += write(1, "0", 1);
-
-                    sprintf(buffer, "%o", num);
-                    count += write(1, buffer, strlen(buffer));
-                }
                     break;
 
                 default:
